f043489bcf0a1eaedee07b7f5421b8a2913e24c8
Alfonso <alfonsolimaastor@gmail.com>
2015-04-18 19:59:05 +0100

    Remove trailing spaces in tests

--- TypeListC++11/test/Misc.cpp
+++ TypeListC++11/test/Misc.cpp
@@ -8,11 +8,11 @@ void TestMisc()
 {
     using Tuple_1= std::tuple<int>;
     using Tuple_2= std::tuple<int, char, int*, const int>;
-            
+
     static_assert( is_instantiation_of<std::tuple, Tuple_1>::value == true, "");
     static_assert( is_instantiation_of<std::tuple, Tuple_2>::value == true, "");
     static_assert( is_instantiation_of<std::tuple, int>::value == false, "");
     static_assert( is_instantiation_of<std::tuple, Empty>::value == false, "");
     static_assert( is_instantiation_of<std::tuple, std::tuple<Empty>>::value == true, "");
-    
+
 }
--- TypeListC++11/test/Transform.cpp
+++ TypeListC++11/test/Transform.cpp
@@ -6,7 +6,7 @@ void TestTransform()
 {
     using Tup = std::tuple<int, float, double*, unsigned int, void*>;
     using Tuple_0 = typename Transform<Tup, MyTranformer>::Result;
-    
+
     static_assert(std::is_same<Tuple_0, std::tuple<A<int>, A<float>, A<double*>, A<unsigned int>, A<void*>>>::value, "");
     static_assert(std::is_same<typename Transform<Empty, MyTranformer>::Result, Empty>::value, "");
     static_assert(std::is_same<typename Transform<std::tuple<A<int>>, MyTranformer>::Result, std::tuple<A<A<int>>> >::value, "");
