aad79f2b6a2cff9496cf7af3cf0d3e8b2d64d90e
Costin Iordache <costinior2001@yahoo.com>
2015-04-20 23:30:01 +0100

    commented out GenData unit test for the time being...

--- TypeListC++11/test/GenData.cpp
+++ TypeListC++11/test/GenData.cpp
@@ -5,7 +5,7 @@
 void TestGenData()
 {
     Root c;
-    c.Set<ClassMember::MEMBER_1>(1);
+    /*c.Set<ClassMember::MEMBER_1>(1);
     c.Set<ClassMember::MEMBER_2>(2.3);
     c.Set<ClassMember::MEMBER_3>(nullptr);
     c.Set<ClassMember::MEMBER_4>(456);
@@ -14,5 +14,5 @@ void TestGenData()
               << c.Get<ClassMember::MEMBER_2>() << std::endl
               << c.Get<ClassMember::MEMBER_3>() << std::endl
               << c.Get<ClassMember::MEMBER_4>() << std::endl
-              << c.Get<ClassMember::MEMBER_5>() << std::endl;
+              << c.Get<ClassMember::MEMBER_5>() << std::endl;*/
 }
